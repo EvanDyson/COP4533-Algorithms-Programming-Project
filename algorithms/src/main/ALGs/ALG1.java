cb29670c4926d88c67e69345beac3866ad1ad0a5
EvanDyson <evandyson@ufl.edu>
2023-03-16 13:22:39 -0400

    update ALG 1 and 2 with comments

--- algorithms/src/main/ALGs/ALG1.java
+++ algorithms/src/main/ALGs/ALG1.java
@@ -4,12 +4,22 @@ package ALGs;
 
 public class ALG1 {
     public static int run_ALG1(int[][] copy) {
+        // initiate a maxprofit for return
         int maxProfit = 0;
-        
+
+        // loop through all the stocks
         for(int i = 0; i < copy.length; i++) {
+
+            // loop through all the days per stock
             for (int j = 0; j < copy[i].length; j++) {
+
+                // loop through all the days after day J
                 for (int k = j+1; k < copy[i].length; k++) {
+
+                    // get the profit if sold at day k - day J
                     int profit = copy[i][k] - copy[i][j];
+
+                    // compare profit to max profit and save the higher integer
                     maxProfit = Math.max(maxProfit, profit);
                 }
             }
--- algorithms/src/main/ALGs/ALG2.java
+++ algorithms/src/main/ALGs/ALG2.java
@@ -3,18 +3,21 @@
 package ALGs;
 
 public class ALG2 {
-    public static int run_ALG2(int[][] copy) {
-        // case 1 where one stock and one day, return 0 profit.
-        // case 2 where one stock and multiple days, return the highest and lowest price day given that the lowest is before the highest
-        // case 3 where more than 1 stock, return the highest different between the highest and lowest price day given that the lowest is before the highest on any stock
-        
+    public static int run_ALG2(int[][] copy) {      
+        // initiate a maxprofit for return
         int maxProfit = 0;
 
+        // loop through all the stocks
         for (int i = 0; i < copy.length; i++) {
+            // initiate a maxprofitperstock and minprice to be reset per stock
             int maxProfitPerStock = 0;
             int minPrice = copy[i][0];
 
+            // loop though all the days per stock
             for (int j = 1; j < copy[i].length; j++) {
+                // if the current price is less than the current saved minPrice then replace min price
+                // otherwise check to see if the current maxProfitPerStock is greater than 
+                // the potential profit if sold at the current day. if it is replace maxProfitPerStock
                 if (copy[i][j] < minPrice)
                     minPrice = copy[i][j];
                 else {
@@ -22,13 +25,12 @@ public class ALG2 {
                     maxProfitPerStock = Math.max(currentProfit, maxProfitPerStock);
                 }
             }
-
             // check if the current stock's max profit is larger than the overall max profit
             // if it is replace it, otherwise continue
             if (maxProfitPerStock > maxProfit)
                 maxProfit = maxProfitPerStock;
         }
-
+        
         System.out.print("|| Algorithm 2: ");       return maxProfit;
     }
 }
